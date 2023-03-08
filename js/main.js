c680d730088379c5b614ee78502955ea9a0d1ec2
mokou <niechenwey@outlook.com>
2023-03-08 21:20:51 +0800

    皮卡堂地图编辑器 v0.0.1 beta

---
+++ js/main.js
@@ -0,0 +1,85 @@
+
+var uw = 20 / 2
+var uh = 10 / 2
+
+document.onkeydown = function(e) {
+    switch(e.key) {
+        case 'd': $('.aim').addClass('delete'); break
+        case 's': $('.aim > img').addClass('mirro'); break
+    }
+}
+
+document.onkeyup = function(e) {
+    switch(e.key) {
+        case 'd': $('.aim').removeClass('delete'); break
+        case 's': $('.aim > img').removeClass('mirro'); break
+    }
+}
+
+document.addEventListener('click', function(e) {
+    if($('.aim').is('.delete')) { 
+        e.stopPropagation()
+        if($(e.target).is('.item, .icon')) {
+            $(e.target).remove()
+        }
+    }
+}, true)
+
+$(document).ready(function() {
+
+
+
+    $('.map-container').mouseout(function(e) {
+        $('.aim').css({visibility: 'hidden'})
+    })
+
+    $('.map-container').mouseover(function(e) {
+        $('.aim').css({visibility: 'visible'})
+    })
+
+    $('.map-container').mousemove(function(e) {
+        $('.aim').css(calcLocate(e))
+    })
+
+    $('.map-container').click(function(e) {
+        $('.map').append($('.aim > .item').clone().css(calcLocate(e)))
+    })
+
+    $('.tool-container').click(function(e) {
+        if($(e.target).is('.icon')) {
+            if($(e.target).is('.selected')) {
+                $('.aim > img').replaceWith($('<img>'))
+            } else {
+                $('.aim > img').replaceWith($(e.target).clone().attr({class: 'item'}))
+                $('.icon.selected').removeClass('selected')
+            }
+            $(e.target).toggleClass('selected')
+            return
+        }
+        $('.upload').click()
+    })
+
+    $('.item').click(function() {
+        console.log('haha')
+    })
+
+    $('.upload').change(function(e) {
+        $(this.files).each(function() {
+            var img = $(`<img class="icon" title="${this.name.match(/(\S*)\./)[1]}">`).appendTo($('.tool-container'))
+            var reader = new FileReader()
+            reader.readAsDataURL(this)
+            reader.addEventListener('load', function() {
+                img.attr({src: reader.result})
+            })
+        })
+        this.value = ''
+    })
+})
+
+function calcLocate(e) {
+    // var tmp = parseInt((e.pageX % uw + e.pageY % uh * 2) / 20) * 0
+    return {
+        left: parseInt((e.pageX - $('.map').offset().left) / uw) * uw - ($('.aim > img').width() - uw / 2) / 2,
+        top: parseInt((e.pageY - $('.map').offset().top) / uh) * uh - ($('.aim > img').height() - uh / 2) / 2,
+    }
+}
